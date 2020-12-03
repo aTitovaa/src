bc353c2be243250a2121b154ba6b8867eb0612af
aTitovaa <74263741+aTitovaa@users.noreply.github.com>
2020-12-03 14:27:36 +0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,66 +1,17 @@
 package com.company;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-
 public class Main {
+    public static class Window{
+        private int cow = 0;
+        private JLabel cowlabel;
 
-    public static class MyWindow {
-
-        private int balance = 0;
-        private JLabel balanceLabel;
-
-
-
-
-        public MyWindow(){
-
-            //Создаем фрейм (окно)
-            JFrame frame = new JFrame("window");
-
-            //Просим программу закрыться при закрытии фрейма
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-            //Создадим лейбл и поместим его во фрейм
-            balanceLabel = new JLabel("Баланс: " + balance);
-            frame.add(balanceLabel, BorderLayout.CENTER);
-
-            //Создадим кнопку "Увеличить" и поместим ее во фрейм
-            JButton btnIncrease = new JButton("Увеличить");
-            frame.add(btnIncrease, BorderLayout.SOUTH);
-            JButton btnIncrease1 = new JButton("Уменьшить");
-            frame.add(btnIncrease1, BorderLayout.NORTH);
-
-
-            //Добавим к кнопке слушатель события
-            btnIncrease1.addActionListener(new ActionListener(){
-                public void actionPerformed(ActionEvent e) {
-                    balance++;
-                    updateBalance();
-                }
-            });
-
-            btnIncrease.addActionListener(new ActionListener(){
-                public void actionPerformed(ActionEvent e) {
-                    balance--;
-                    updateBalance();
-                }
-            });
-
-            //Установка размеров окна
-            frame.pack();
-            frame.setVisible(true);
+        public Window(){
+            JFrame
         }
 
-        private void updateBalance () {
-            //Обновляем содержимое Лейбла
-            balanceLabel.setText("Баланс: " + balance);
-        }
 
-        public static void main(String[] args) {
-            MyWindow app = new MyWindow();
-        }
-    }}
+    public static void main(String[] args) {
 
+    }
+}}
